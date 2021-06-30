ee3af33afae9050697a6c9316b7a1dfbf083632a
r00tDada <sourabh18299@gmail.com>
2021-06-30 17:13:17 +0530

    cypress_first_commit

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -1,6 +1,6 @@
 ///    <reference types="cypress" />
 describe('My First Test', () => {
     it('Does not do much!', () => {
-      expect(true).to.equal(true)
+    //   expect(true).to.equal(true)
     })
   })
