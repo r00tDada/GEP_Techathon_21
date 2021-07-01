4cc802472810bc984a1f8d01eb583259b888d6f5
Smitpatel-gep <smit_patel@gep.com>
2021-07-01 20:19:20 +0530

    Main Logic of Localization completed

--- cypress/integration/Basic Tests/Basic.js
+++ cypress/integration/Basic Tests/Basic.js
@@ -1,56 +1,35 @@
 ///    <reference types="cypress" />
 
-var culture 
-
 describe('Basic Tests', () => {
 
     before(function(){
+
         cy.visit("https://r00tdada.github.io/GEP_Techathon_21/web/Blunder's%20Pride.html")
-        // var s ='culture'
+        
         cy.fixture('culture').then((culture) => {
+
             for(var language in culture){
-                var lang = cy.get('#country')
-                if(lang.should('have.value', language)){
-                    var s=culture[language].toString().toLowerCase()
-                    cy.fixture(s).then((langg)=>{
-                        this.langg=langg
+
+                var selectedLanguage = cy.get('#country')
+
+                if(selectedLanguage.should('have.value', language)){
+
+                    cy.fixture(culture[language].toString().toLowerCase()).then((currentLanguage)=>{
+                        this.cul=currentLanguage
                     })
-                    cy.log(this.langg)
+
                     break
                 }
-            };
-          })
+            }
+        })
     })
 
-    beforeEach( () => {
+    beforeEach(function(){
         cy.visit("https://r00tdada.github.io/GEP_Techathon_21/web/Blunder's%20Pride.html")
     });
 
-    it('Fixture Load & Test', function() { 
-        // cy.get('#country').select('German')
-        // cy.wait(1000)
-        // cy.get('#country').select('Hindi')
-        // if(cy.get('#country').should('have.value', '02')){
-        //     cy.log("Hindiiiiiii")
-        // }
-        // else if(cy.get('#country').should('have.value', '01')){
-        //     cy.log("English")
-        // }
-        cy.log(culture)
-        // cy.get('#pagetype').should('have.value',culture.pageType)
-        // cy.get('#pagetype').should('not.have.value',culture.pageTitle)
-        // expect(this.english, 'fixture in the test context').to.deep.equal(english)
-    })
-
-    // it('Title Test', function() {
-    //     cy.title().should('contain',"Automate")
-    //     cy.title().should('not.contain','Cypress')
-    // });
-
-    // it('Home Page Loading Test', function() {
-    //     cy.get('#pageTitle').should('contain',this.english.pageTitle)
-    //     cy.get('#pageType').should('contain',this.english.pageType)
-    //     cy.get('.lead').should('contain','GEP')
-    // });
+    it('Home Page Loading Test', function() {
+        cy.get('#pagetype').should('have.text',this.cul.pageType)
+    });
 
   })
