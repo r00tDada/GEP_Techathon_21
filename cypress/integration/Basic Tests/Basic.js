859f62ba21d579a1baf9a75108fb0294f0e4951d
r00tDada <sourabh18299@gmail.com>
2021-07-02 02:41:21 +0530

    test_case

--- cypress/integration/Basic Tests/Basic.js
+++ cypress/integration/Basic Tests/Basic.js
@@ -2,7 +2,7 @@
 
 describe('Basic Tests', () => {
 
-    before(function(){
+    beforeEach(function(){
 
         cy.visit("https://r00tdada.github.io/GEP_Techathon_21/web/Blunder's%20Pride.html")
         
@@ -24,12 +24,51 @@ describe('Basic Tests', () => {
         })
     })
 
-    beforeEach(function(){
-        cy.visit("https://r00tdada.github.io/GEP_Techathon_21/web/Blunder's%20Pride.html")
-    });
-
     it('Home Page Loading Test', function() {
+        cy.wait(1000)
         cy.get('#pagetype').should('have.text',this.cul.pageType)
+        cy.wait(1000)
     });
 
+    it('Testing First Name Block',function(){
+        cy.wait(1000)
+        cy.get('#fname').should('have.text',this.cul.firstName)
+
+    });
+
+    it('Testing Last Name Block',function(){
+        cy.wait(1000)
+        cy.get('#lname').should('have.text',this.cul.LastName)
+    });
+
+    it('Testing Email',function(){
+        cy.wait(1000)
+        cy.get('#email').should('have.text',this.cul.Email);
+    });
+
+    it('Testing Mobile',function(){
+        cy.wait(1000)
+        cy.get('#mobile').should('have.text',this.cul.Mobile);
+    });
+
+    it('Testing Institute',function(){
+        cy.wait(1000)
+        cy.get('#inst').should('have.text',this.cul.Institute);
+    });
+    // it('Testing Graduation Year',function(){
+    //     cy.wait(1000)
+    //     cy.get('#Gradyr').should('have.text',this.cul.Gradyr);
+    // });
+    // it('Testing Project Category',function(){
+    //     cy.wait(1000)
+    //     cy.get('#pcat').should('have.text',this.cul.Projectcat);
+    // });
+    // it('Testing Institute',function(){
+    //     cy.wait(1000)
+    //     cy.get('#inst').should('have.text',this.cul.Institute);
+    // });
+    // it('Testing Institute',function(){
+    //     cy.wait(1000)
+    //     cy.get('#inst').should('have.text',this.cul.Institute);
+    // });
   })
