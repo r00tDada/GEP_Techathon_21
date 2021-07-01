4a6eba8a98ab5051c1b3e0f08431d1b7a57140f2
r00tDada <sourabh18299@gmail.com>
2021-07-01 20:28:11 +0530

    test

--- cypress/integration/Basic Tests/Basic.js
+++ cypress/integration/Basic Tests/Basic.js
@@ -1,24 +1,16 @@
 ///    <reference types="cypress" />
 
-var culture 
+const english = require('../../fixtures/english.json') 
 
 describe('Basic Tests', () => {
 
-<<<<<<< HEAD
-    before( () => {
-=======
     before(function(){
         cy.visit("https://r00tdada.github.io/GEP_Techathon_21/web/Blunder's%20Pride.html")
-        // var s ='culture'
         cy.fixture('culture').then((culture) => {
             for(var language in culture){
                 var lang = cy.get('#country')
                 if(lang.should('have.value', language)){
-                    var s=culture[language].toString().toLowerCase()
-                    cy.fixture(s).then((langg)=>{
-                        this.langg=langg
-                    })
-                    cy.log(this.langg)
+                    this.lang = cy.fixture(language)
                     break
                 }
             };
@@ -26,31 +18,20 @@ describe('Basic Tests', () => {
     })
 
     beforeEach( () => {
->>>>>>> 4635869f516b697c1d5617ca59ea17510662fe08
         cy.visit("https://r00tdada.github.io/GEP_Techathon_21/web/Blunder's%20Pride.html")
     });
 
-<<<<<<< HEAD
-    it('Fixture Load & Test', function() {
-        cy.get('[id="country"]').click().get('[value='001']').click()
-        // cy.get('select').click()
-        // expect(this.lang, 'fixture in the test context').to.deep.equal(selectlang)
-=======
     it('Fixture Load & Test', function() { 
-        // cy.get('#country').select('German')
-        // cy.wait(1000)
-        // cy.get('#country').select('Hindi')
-        // if(cy.get('#country').should('have.value', '02')){
-        //     cy.log("Hindiiiiiii")
-        // }
-        // else if(cy.get('#country').should('have.value', '01')){
-        //     cy.log("English")
-        // }
-        cy.log(culture)
-        // cy.get('#pagetype').should('have.value',culture.pageType)
-        // cy.get('#pagetype').should('not.have.value',culture.pageTitle)
+        cy.get('#country').select('German')
+        cy.wait(1000)
+        cy.get('#country').select('Hindi')
+        if(cy.get('#country').should('have.value', '02')){
+            cy.log("Hindiiiiiii")
+        }
+        else if(cy.get('#country').should('have.value', '01')){
+            cy.log("English")
+        }
         // expect(this.english, 'fixture in the test context').to.deep.equal(english)
->>>>>>> 4635869f516b697c1d5617ca59ea17510662fe08
     })
 
     // it('Title Test', function() {
