19175283dfa982bf1444c376e29a9253e2415216
r00tDada <sourabh18299@gmail.com>
2021-07-01 19:43:25 +0530

    cmm

--- cypress/integration/Basic Tests/Basic.js
+++ cypress/integration/Basic Tests/Basic.js
@@ -4,24 +4,26 @@ const english = require('../../fixtures/english.json')
 
 describe('Basic Tests', () => {
 
-    beforeEach( () => {
+    before( () => {
         cy.visit("https://r00tdada.github.io/GEP_Techathon_21/web/Blunder's%20Pride.html")
         cy.fixture('english.json').as('english')
     });
 
     it('Fixture Load & Test', function() {
-        expect(this.english, 'fixture in the test context').to.deep.equal(english)
+        cy.get('[id="country"]').click().get('[value='001']').click()
+        // cy.get('select').click()
+        // expect(this.lang, 'fixture in the test context').to.deep.equal(selectlang)
     })
 
-    it('Title Test', function() {
-        cy.title().should('contain',"Automate")
-        cy.title().should('not.contain','Cypress')
-    });
+    // it('Title Test', function() {
+    //     cy.title().should('contain',"Automate")
+    //     cy.title().should('not.contain','Cypress')
+    // });
 
-    it('Home Page Loading Test', function() {
-        cy.get('#pageTitle').should('contain',this.english.pageTitle)
-        cy.get('#pageType').should('contain',this.english.pageType)
-        cy.get('.lead').should('contain','GEP')
-    });
+    // it('Home Page Loading Test', function() {
+    //     cy.get('#pageTitle').should('contain',this.english.pageTitle)
+    //     cy.get('#pageType').should('contain',this.english.pageType)
+    //     cy.get('.lead').should('contain','GEP')
+    // });
 
   })
