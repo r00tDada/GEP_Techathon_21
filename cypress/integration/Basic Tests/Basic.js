6058e96a284cdc6d35b63274fd5367229b8e8134
r00tDada <sourabh18299@gmail.com>
2021-07-02 14:01:33 +0530

    add id of pcat

--- cypress/integration/Basic Tests/Basic.js
+++ cypress/integration/Basic Tests/Basic.js
@@ -133,40 +133,52 @@ describe('Basic Tests', () => {
     //     cy.log('**checking field name**')c
     //     cy.get('#gradyr').should('have.text',this.cul.Gradyr)
     //     cy.wait(1000)
+    // cy.log('**checking inputs year**')
+    // cy.get('#gradyear').find('option').then(options => {
+    //     const yr = [...options].map(o => o.text)
+    //     expect(yr).to.deep.eq([this.cul.selectyr1,this.cul.selectyr2,this.cul.selectyr3,this.cul.selectyr4])
+    // })
+    //     cy.wait(1000)
     //     cy.log("Selecting graduation yrs")
     //     cy.get('#gradyear').select(this.cul.selectyr2)
     //     cy.wait(1500)
     // });
-    it('Testing Project Category and radio',function(){
+    it('Testing Project Category and radio', function () {
         cy.wait(1000)
         cy.log('**checking field name**')
-        cy.get('#pcat').should('have.text',this.cul.Projectcat);
+        cy.get('#pcat').should('have.text', this.cul.Projectcat);
+        cy.wait(1000)
+        cy.log('**checking category name**')
+        cy.get('#pcattype').find('label').then(label => {
+            const yr = [...label].map(o => o.text)
+            expect(yr).to.deep.eq([this.cul.category1,this.cul.category2,this.cul.category3,this.cul.category4])
+        })
         cy.wait(1200)
         cy.log('**Initially checking radio button')
         cy.get('#inpcat1').should('be.visible').should('not.be.checked')
         cy.get('#inpcat2').should('be.visible').should('be.checked')
         cy.get('#inpcat3').should('be.visible').should('not.be.checked')
         cy.wait(1200)
-        cy.log('Checking radio button '+this.cul.category3)
+        cy.log('Checking radio button ' + this.cul.category3)
         cy.get('#inpcat3').click()
         cy.get('#inpcat1').should('be.visible').should('not.be.checked')
         cy.get('#inpcat2').should('be.visible').should('not.be.checked')
         cy.get('#inpcat3').should('be.visible').should('be.checked')
         cy.wait(1500)
     });
-    it('Testing Project Idea Block',function(){
-        cy.wait(1000)
-        cy.get('#idea').should('have.text',this.cul.idea);
-        cy.wait(1000)
-        cy.log('**checking no. of inputs**')
-        cy.get('#cc-number').should('have.length', 1)
-        cy.wait(1000)
-        cy.log('**checking if it is empty or not**')
-        cy.get('input[id=cc-number]').invoke('val').should('be.empty')
-        cy.wait(1000)
-        cy.log('Enter the project idea')
-        cy.get('#cc-number').type(this.cul.projectidea, { delay: 100 })
-        cy.wait(1500)
-    });
+    // it('Testing Project Idea Block',function(){
+    //     cy.wait(1000)
+    //     cy.get('#idea').should('have.text',this.cul.idea);
+    //     cy.wait(1000)
+    //     cy.log('**checking no. of inputs**')
+    //     cy.get('#cc-number').should('have.length', 1)
+    //     cy.wait(1000)
+    //     cy.log('**checking if it is empty or not**')
+    //     cy.get('input[id=cc-number]').invoke('val').should('be.empty')
+    //     cy.wait(1000)
+    //     cy.log('Enter the project idea')
+    //     cy.get('#cc-number').type(this.cul.projectidea, { delay: 100 })
+    //     cy.wait(1500)
+    // });
 
 })
