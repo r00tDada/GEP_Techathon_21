24ea40aa8320e8eb8d6a16ecedebb0d672fc3448
r00tDada <sourabh18299@gmail.com>
2021-07-02 11:29:33 +0530

    added placeholder in first 3

--- cypress/integration/Basic Tests/Basic.js
+++ cypress/integration/Basic Tests/Basic.js
@@ -24,50 +24,64 @@ describe('Basic Tests', () => {
         })
     })
 
-    it('Home Page Loading Test', function () {
-        cy.wait(1000)
-        cy.get('#pagetype').should('have.text', this.cul.pageType)
-    });
+    // it('Home Page Loading Test', function () {
+    //     cy.wait(1000)
+    //     cy.get('#pagetype').should('have.text', this.cul.pageType)
+    // });
 
-    it.only('Testing First Name Block', function () {
+    // it('Testing First Name Block', function () {
+    //     cy.wait(1000)
+    //     cy.log('**checking field name**')
+    //     cy.get('#fname').should('have.text', this.cul.firstName)
+    //     cy.wait(1000)
+    //     cy.log('**checking placeholder**')
+    //     cy.get('#firstName').invoke('attr', 'placeholder').should('contain', this.cul.firstnameplaceholder)
+    //     cy.wait(1000)
+    //     cy.log('**checking no. of inputs**')
+    //     cy.get('#firstName').should('have.length',1)
+    //     cy.wait(1000)
+    //     cy.log('**checking if it is empty or not**')
+    //     cy.get('input[id=firstName]').invoke('val').should('be.empty')
+    //     cy.wait(1000)
+    //     cy.log('Enter the first name')
+    //     cy.get('#firstName').type(this.cul.userfirstname,{delay:100}) 
+    // });
+
+    // it('Testing Last Name Block', function () {
+    //     cy.wait(1000)
+    //     cy.log('**checking field name**')
+    //     cy.get('#lname').should('have.text', this.cul.lastName)
+    //     cy.wait(1000)
+    //     cy.log('**checking placeholder**')
+    //     cy.get('#lastName').invoke('attr', 'placeholder').should('contain', this.cul.lastnameplaceholder)
+    //     cy.wait(1000)
+    //     cy.log('**checking no. of inputs**')
+    //     cy.get('#lastName').should('have.length',1)
+    //     cy.wait(1000)
+    //     cy.log('**checking if it is empty or not**')
+    //     cy.get('input[id=lastName]').invoke('val').should('be.empty')
+    //     cy.wait(1000)
+    //     cy.log('Enter the last name')
+    //     cy.get('#lastName').type(this.cul.userlastname,{delay:100}) 
+    // });
+
+    it('Testing Email Block', function () {
+        
         cy.wait(1000)
         cy.log('**checking field name**')
-        cy.get('#fname').should('have.text', this.cul.firstName)
-        cy.wait(1000)
-        cy.log('**checking placeholder**')
-        cy.get('#firstName').invoke('attr', 'placeholder').should('contain', 'userfirstname')
-        cy.wait(1000)
-        cy.log('**checking no. of inputs**')
-        cy.get('#firstName').should('have.length',1)
-        cy.wait(1000)
-        cy.log('**checking if it is empty or not**')
-        cy.get('input[id=firstName]').invoke('val').should('be.empty')
-        cy.wait(1000)
-        cy.log('Enter the name')
-        cy.get('#firstName').type(this.cul.userfirstname,{delay:100}) 
-    });
-
-    it('Testing Last Name Block', function () {
-        cy.wait(1000)
-        cy.log('**checking field name**')
-        cy.get('#lname').should('have.text', this.cul.firstName)
-        cy.wait(1000)
-        cy.log('**checking placeholder**')
-        cy.get('#lastName').invoke('attr', 'placeholder').should('contain', 'userlastname')
-        cy.wait(1000)
-        cy.log('**checking no. of inputs**')
-        cy.get('#lastName').should('have.length',1)
-        cy.wait(1000)
-        cy.log('**checking if it is empty or not**')
-        cy.get('input[id=lastName]').invoke('val').should('be.empty')
-        cy.wait(1000)
-        cy.log('Enter the name')
-        cy.get('#lastName').type(this.cul.userlastname,{delay:100}) 
-    });
-
-    it('Testing Email', function () {
-        cy.wait(1000)
         cy.get('#email').should('have.text', this.cul.Email);
+        cy.wait(1000)
+        cy.log('**checking placeholder**')
+        cy.get('#emailid').invoke('attr', 'placeholder').should('contain', this.cul.emailplaceholder)
+        cy.wait(1000)
+        cy.log('**checking no. of inputs**')
+        cy.get('#emailid').should('have.length', 1)
+        cy.wait(1000)
+        cy.log('**checking if it is empty or not**')
+        cy.get('input[id=emailid]').invoke('val').should('be.empty')
+        cy.wait(1000)
+        cy.log('Enter the email')
+        cy.get('#emailid').type(this.cul.email, { delay: 100 })
     });
 
     it('Testing Mobile', function () {
