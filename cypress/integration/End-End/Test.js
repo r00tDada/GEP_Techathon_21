7f24ca022b26aaf46667970aea569bda4be3b41a
C2OO13 <smitpat2000@gmail.com>
2021-07-03 10:37:46 +0530

    feat: Stored tests in commands.js

--- cypress/integration/End-End/Test.js
+++ cypress/integration/End-End/Test.js
@@ -1,34 +1,33 @@
 ///    <reference types="cypress" />
+const cul = require('../../fixtures/culture.json')
 
-describe('End to End Tests', () => {
+var lan = []
+var lang = []
 
-    beforeEach(function(){
-        cy.visit("https://r00tdada.github.io/GEP_Techathon_21/web/Blunder's%20Pride.html")
-        cy.fixture('culture').then((culture) => {
-            for(var language in culture){
-                if(cy.get('#country').should('have.value', language)){
-                    cy.fixture(culture[language].toString().toLowerCase()).then((currentLanguage)=>{
-                        this.cul=currentLanguage
-                    })
-                    break
-                }
-            }
+for (var index in cul) {
+    lan.push(index)
+    lang.push(cul[index])
+}
+
+lang.forEach((value) => {
+    describe('End to End Test for ' + value, () => {
+
+        beforeEach(function () {
+            cy.visit("https://r00tdada.github.io/GEP_Techathon_21/web/Blunder's%20Pride.html")
+            cy.get('#country').select(value)
+            cy.fixture(value.toString().toLowerCase()).then((currentLanguage) => {
+                this.cul = currentLanguage
+
+            })
+
         })
-    });
 
-    it('Entire Workflow Check for Selected Language', function() {
-        cy.get('#pagetype').should('have.text',this.cul.pageType)
-        cy.get('#firstName').type('Test User')
-        cy.get('#lastName').type('@GEP')
-        cy.get('#emailid').type('test_user@gep.com')
-        cy.get('#mobileno').type('+918780658153')
-        cy.get('#instname').type('Work From Home')
-        cy.get('#gradyear').select('2022')
-        cy.get('#cat2').click()
-        cy.get('#cc-number').type('Localization Automation Testing')
-        cy.get('#save-info').click()
-        cy.get('#sub').click()
-        cy.on('window:confirm',()=>{})
-    });
+        it('Entire Workflow Check', function() {
+            cy.Workflow_Check(this.cul)
+        });
 
-  })
+        
+    });
+});
+
+    
